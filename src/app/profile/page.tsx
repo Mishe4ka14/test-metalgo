431ac529beec4f7b60959e0ee3da6ec3afda7900
Mishe4ka14 <mishechka.14@yandex.ru>
2024-10-01 17:33:38 +0300

    fix tailwind and postcss config, add registr, profile pages

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+  return(
+    <div className="min-h-8 min-w-8 bg-red-600">
+      <p className="font-black">Шваклбакл</p>
+      <p>peq</p>
+    </div>
+  )
+}
+
+export default Profile;
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,66 @@
-const Register = ():JSX.Element => {
+'use client';
+
+import type {NextPage} from 'next'
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+interface FormValues {
+  name: string;
+  email: string;
+  password: string;
+}
+// схема валидации для yup
+const schema = yup.object().shape({
+  name: yup.string().required("Имя обязательно"),
+  email: yup.string().email("Некорректный email").required("Email обязателен"),
+  password: yup.string().min(6, "Минимум 6 символов").required("Пароль обязателен"),
+});
+
+const Register: NextPage = () => {
+
+  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data: FormValues) => {
+    //сохраняем данные
+    sessionStorage.setItem('userData', JSON.stringify(data));
+    
+    console.log(data);
+  };
+
+
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h2 className="text-2xl font-semibold">Регистрация</h2>
+    <div className="flex justify-center items-center mt-[25%]">
+        <h2 className="text-2xl font-semibold">Регистрация</h2>
+        <form 
+          onSubmit={handleSubmit(onSubmit)} 
+          className='flex flex-col items-start gap-10'
+          >
+          <div>
+            <label>Имя</label>
+            <input {...register("name")}/>
+            {errors.name && <p>{errors.name.message}</p>}
+          </div>
+          <div>
+            <label>Email</label>
+            <input {...register("email")}/>
+            {errors.email && <p>{errors.email.message}</p>}
+          </div>
+          <div>
+            <label>Пароль</label>
+            <input {...register("password")}/>
+            {errors.password && <p>{errors.password.message}</p>}
+          </div>
+          <button type='submit'>Отправить</button>
+        </form>
     </div>
   )
 }
